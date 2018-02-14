c5d489e2370b42298de7cc04b09113a6acb1d4ab
unknown <cx7ga@virginia.edu>
2018-02-14 01:09:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var questions=[new Question("Who is the founder of Facebook",["Mark Zuckerberg","Steve Jobs"],"Mark Zuckerberg"),
+               new Question("Calculate 2*7+2",[16,12],16)];
+
+
+var quiz=new Quiz(questions);
+
+
+
+QuizUI.displayNext();
